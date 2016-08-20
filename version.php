fc1bc5e7268790dcbbe61e19af538275ff076740
Sarah Street <sarah@officegrader.com>
2016-08-19 19:11:45 -0500

    Require Moodle 2.8 in version.php because get_course_and_cm_from_cmid() is undefined in earlier versions

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_og';
 $plugin->version = 2016032400;
 $plugin->release = 'v0.1';
-$plugin->requires = 2010112400;
+$plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->cron = 0;
 $plugin->dependencies = array();
